61451f2694cccde5135a90d4fa34036cb11c8ff7
SomethingWitty295 <56739116+SomethingWitty295@users.noreply.github.com>
2022-10-20 19:55:47 -0400

    Added needed gettext_lazy import statement.

--- recommender/models.py
+++ recommender/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 class Musicdata(models.Model):
     track_id = models.TextField()
