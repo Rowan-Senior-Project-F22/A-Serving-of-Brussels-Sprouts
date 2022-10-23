9f6d9e7ae1adf76057d312d33ddeab40c1fe2e9b
ny247 <alzura47@students.rowan.edu>
2022-10-23 19:52:55 -0400

    modified files.

--- recommender/urls.py
+++ recommender/urls.py
@@ -4,11 +4,7 @@ from . import views
 app_name = 'recommender'
 
 urlpatterns = [
-    path('artist/', views.get_artist, name='get_artist'),
-    path('album/', views.get_album, name='get_album'),
-    path("register/", views.get_registration, name="get_registration"),
-    path("login/", views.get_login, name="get_login"),
-    path("", views.get_landing_guest, name="get_landing_guest")
+    path("", views.get_landing_guest, name="get_landing_guest"),
     path("profile/<int:user_id>", views.user_profile , name="profile"),
 ]
  
