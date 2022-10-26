850b7a2c7eee49d638aaa327416e23949fedef35
Wasiu Biobaku <Wasiu7890@outlook.com>
2022-10-26 12:24:06 -0400

    Clean up
    Removed some unnecessary things to make merging easier. Got rid of my own login form and signup form.

--- recommender/admin.py
+++ recommender/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Musicdata
-
+from .models import ThreadModel
 # Register your models here.
-admin.site.register(Musicdata)
+admin.site.register(ThreadModel)
+
--- recommender/urls.py
+++ recommender/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from .views import CreateThread, ListThreads,ThreadView,CreateMessage
-from .import views
-from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('' ,views.frontpage, name = 'frontpage'),
     path('inbox/', ListThreads.as_view(), name = 'inbox'),
     path('inbox/create-thread', CreateThread.as_view(), name= 'create-thread'),
     path('inbox/<int:pk>', ThreadView.as_view(), name = 'thread'),
