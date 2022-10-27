84546df1ebf6f3250f75a64c8b8b20a2df373a82
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-10-27 19:00:45 -0400

    Deployment prep

--- music/settings.py
+++ music/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
 ]
 
 MIDDLEWARE = [
@@ -122,9 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/recommender/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
