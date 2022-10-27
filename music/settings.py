41a070eead58f3ea47645fec5fc1a7b460a14406
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-10-27 18:45:46 -0400

    Themef

--- music/settings.py
+++ music/settings.py
@@ -126,12 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/recommender/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
