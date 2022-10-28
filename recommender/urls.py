c413709515f2a58ffbd981c7904274b317a9e9c4
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-10-27 21:12:22 -0400

    Deploy

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from .views import CreateThread, ListThreads,ThreadView,CreateMessage
-from .import views
-from django.contrib.auth import views as auth_views
+from .views import CreateThread, ListThreads, ThreadView, CreateMessage
+from . import views
 
+app_name = 'recommender'
 
 urlpatterns = [
-    path('inbox/', ListThreads.as_view(), name = 'inbox'),
-    path('inbox/create-thread', CreateThread.as_view(), name= 'create-thread'),
-    path('inbox/<int:pk>', ThreadView.as_view(), name = 'thread'),
-    path('inbox/<int:pk>/create-message', CreateMessage.as_view(), name = 'create-message'),
+    path('inbox/', ListThreads.as_view(), name='inbox'),
+    path('inbox/create-thread', CreateThread.as_view(), name='create-thread'),
+    path('inbox/<int:pk>', ThreadView.as_view(), name='thread'),
+    path('inbox/<int:pk>/create-message', CreateMessage.as_view(), name='create-message'),
     path("", views.get_landing_guest, name="get_landing_guest"),
-    path("profile/<int:user_id>", views.user_profile , name="profile"),
-    path("playlist/<int:user_id>", views.user_playlist, name="profile"),
-    path("l_room/<str:room_name>", views.l_room, name = "l_room")
+    path("profile/<int:user_id>", views.user_profile, name="profile"),
+    path("playlist/<int:user_id>", views.user_playlist, name="playlist"),
+    path("l_room/<str:room_name>", views.l_room, name="l_room")
 ]
- 
