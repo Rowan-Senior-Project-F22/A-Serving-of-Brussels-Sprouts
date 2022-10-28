6bed6c4b2696833134e49ed589702a76126eec85
V01D <JeremyJuckett@Gmail.com>
2022-10-28 15:26:11 -0400

    Fixed Some Issues
    Changed the reference to 'user_profile' in base.html.
    Temporarily remvoed the int-arg from the user_profile url in urls.py.
    Made migrations.

--- recommender/urls.py
+++ recommender/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('inbox/<int:pk>', ThreadView.as_view(), name='thread'),
     path('inbox/<int:pk>/create-message', CreateMessage.as_view(), name='create-message'),
     path("", views.get_landing_guest, name="get_landing_guest"),
-    path("profile/<int:user_id>", views.user_profile, name="profile"),
+    path("profile/", views.user_profile, name="profile"),
     path("playlist/<int:user_id>", views.user_playlist, name="playlist"),
     path("l_room/<str:room_name>", views.l_room, name="l_room")
 ]
