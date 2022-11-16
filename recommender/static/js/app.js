f00dca60624169812444564130ffa71a9009f504
SomethingWitty295 <56739116+SomethingWitty295@users.noreply.github.com>
2022-11-16 17:05:31 -0500

    Access Token and Refresh Token Obtained
    Working Javascript implementation that successfully obtains a user's access and refresh tokens.

---
+++ recommender/static/js/app.js
@@ -0,0 +1,107 @@
+var redirect_uri = "http://127.0.0.1:8000/";
+
+var client_id = "2de1575d99b14786ae4f7e46e33e494e";
+var secret_id = "fbf315776bda4ea2aaeeeb1ec559de7d";
+
+var access_token = null;
+var refresh_token = null;
+var currentPlaylist = "";
+var radioButtons = [];
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token";
+const USERPROFILE = "https://api.spotify.com/v1/me";
+const PLAYLISTS = "https://api.spotify.com/v1/me/playlists";
+const DEVICES = "https://api.spotify.com/v1/me/player/devices";
+const PLAY = "https://api.spotify.com/v1/me/player/play";
+const PAUSE = "https://api.spotify.com/v1/me/player/pause";
+const NEXT = "https://api.spotify.com/v1/me/player/next";
+const PREVIOUS = "https://api.spotify.com/v1/me/player/previous";
+const PLAYER = "https://api.spotify.com/v1/me/player";
+const TRACKS = "https://api.spotify.com/v1/playlists/{{PlaylistId}}/tracks";
+const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
+const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle";
+
+function onPageLoad() {
+    client_id = localStorage.getItem("client_id");
+    secret_id = localStorage.getItem("secret_id");
+    if (window.location.search.length > 0) {
+        handleRedirect();
+    }
+}
+
+function handleRedirect() {
+    let code = getCode();
+    fetchAccessToken(code);
+    window.history.pushState("", "", redirect_uri); // remove param from url
+}
+
+function getCode() {
+    let code = null;
+    const queryString = window.location.search;
+    if (queryString.length > 0) {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+}
+
+function fetchAccessToken(code) {
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&secret_id=" + secret_id;
+    callAuthorizationApi(body);
+}
+
+function refreshAccessToken() {
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + secret_id));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse() {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if (data.access_token != undefined) {
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if (data.refresh_token != undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function requestAuthorization() {
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("secret_id", secret_id); // In a real app you should not expose your secret_id to the user
+
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url; // Show Spotify's authorization screen
+}
