c0ef49f80eff13db34cb94d60dd0c4a0d353cc0e
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-12-12 12:15:35 -0500

    Model/database changes

--- recommender/models.py
+++ recommender/models.py
@@ -7,9 +7,11 @@ from django.utils.translation import gettext_lazy as _
 - Brandon Ngo'''
 class User(AbstractUser):
     email = models.EmailField(_('email'), unique=True)
-    profile_picture = models.ImageField(null=True, blank=True, upload_to="profile/")
+    profile_picture = models.CharField(null=False, default="", max_length=9999)
     preferences = models.CharField(null=False, default='{}', max_length=1000)
     following = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    friend_count = models.IntegerField(default=0)
+    friends_list = models.ManyToManyField('self')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
