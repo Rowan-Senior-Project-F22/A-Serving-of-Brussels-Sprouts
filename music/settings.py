1d15b4ec39591e4be55c8a56b6d2f93f129c2042
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-12-14 13:55:17 -0500

    Staging prep test

--- music/settings.py
+++ music/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/recommender/static/'
+STATIC_ROOT = ''
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ('static',)
 
 CHANNEL_LAYERS = {
     "default": {
