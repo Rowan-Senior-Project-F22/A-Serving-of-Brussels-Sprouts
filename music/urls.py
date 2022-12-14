1193ebcc8b0c7371ec041ff5b37bd98943cd0537
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-12-14 14:29:08 -0500

    5

--- music/urls.py
+++ music/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recommender.urls')) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-]
+    path('', include('recommender.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
